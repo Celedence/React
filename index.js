cec69cff0e2383546d126402d17d44f5fbacd210
brian <brianbb174@protonmail.com>
2020-07-27 10:50:34 -0700

    intial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+class Hello extends React.Component {
+    render(){
+        return (
+        <div>
+            <h1>Hello There</h1>
+            <h1>Hello There</h1>
+            <h1>Hello There</h1>
+        </div>
+        );
+    }
+}
+
+// function Hello() {
+//     return (
+//         <div>
+//             <h1>Hello!!! there</h1>
+//             <h1>Hello there</h1>
+//             <h1>Hello there</h1>
+//         </div>
+//     );
+// }
+
+
+
+ReactDOM.render(<Hello/>, document.getElementById('root'));
